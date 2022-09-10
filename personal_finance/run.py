b9e8561ac8dd1e6bb43e8b35a5a8698e910e8cb5
Abhishek Patel <50932225+abhishek815@users.noreply.github.com>
2022-09-10 12:59:21 -0400

    list filtered finance data

--- personal_finance/run.py
+++ personal_finance/run.py
@@ -17,11 +17,27 @@ def main(year, file_dir):
 
     finance_types = [income.Income(), investments.Investments(), spending.Spending()]
 
+    months_df_dict = {}
     for finance_type in finance_types:
         file_name = f"{finance_type.name}_monthly_aggregated.csv"
+
         finance_df = finance_type.get_aggregated_monthly(year=str(year))
+        months_df_dict[finance_type.name] = [
+            finance_type.transform(month=month, year=year) for month in constants.MONTHS
+        ]
+
+        # ensure paths are present
         utils.check_dir(file_dir)
+        utils.check_dir(os.path.join(file_dir, finance_type.name))
+
+        # aggregated finance df's and transaction data saved in designated directory
         finance_df.to_csv(os.path.join(file_dir, file_name))
+        utils.list_df_to_directory(
+            months_df_dict[finance_type.name],
+            finance_type.name,
+            os.path.join(file_dir, finance_type.name),
+            offset=1,
+        )
 
 
 if __name__ == "__main__":
--- personal_finance/utils/utils.py
+++ personal_finance/utils/utils.py
@@ -1,7 +1,7 @@
 import os
 
 
-def determine_month(month: int):
+def determine_month(month: int) -> str:
     if month >= 1 and month < 10:
         return f"0{month}"
     elif month >= 10 and month <= 12:
@@ -10,6 +10,14 @@ def determine_month(month: int):
         raise Exception("Invalid month value, please enter a value between 1 and 12")
 
 
-def check_dir(file_dir):
+def check_dir(file_dir) -> None:
     if not os.path.isdir(file_dir):
         os.makedirs(file_dir)
+
+
+def list_df_to_directory(
+    dfs: list, finance_type_name: str, dir_name: str, offset: int
+) -> None:
+    for i, df in enumerate(dfs):
+        file_name = os.path.join(dir_name, f"{finance_type_name}_{offset+i}.csv")
+        df.to_csv(file_name)
